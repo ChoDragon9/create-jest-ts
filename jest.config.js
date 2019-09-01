509e1e52c0b65940ff9e4978a86ccb0d6d1479a1
peter.cho <peter.cho@kakaocorp.com>
2019-09-01 14:47:39 +0900

    인텐트 수정

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    "transform": {
-        "^.+\\.ts?$": "ts-jest"
-    },
-    "testRegex": "\\.spec\\.ts$",
-    "globals": {
-        "ts-jest": {
-            "diagnostics": true
-        }
+  "transform": {
+    "^.+\\.ts?$": "ts-jest"
+  },
+  "testRegex": "\\.spec\\.ts$",
+  "globals": {
+    "ts-jest": {
+      "diagnostics": true
     }
+  }
 }
